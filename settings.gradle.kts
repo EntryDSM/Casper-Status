9482b1e575f855c34d2d677e2102baff9d773c32
박주원 <qkrwndnjs7308@gmail.com>
2025-05-11 23:14:03 +0900

    refactor ( #2 ) : 루트 gradle 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "Casper-Status"
+
+pluginManagement {
+    includeBuild("casper-convention")
+    includeBuild("build-logic")
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
