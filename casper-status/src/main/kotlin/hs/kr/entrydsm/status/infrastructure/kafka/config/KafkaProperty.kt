0dd7027cfd3f4f235325084954905a6384f3dea3
coehgns <modooboiroo@gmail.com>
2025-09-04 18:31:18 +0900

    feat ( #21 ) : KafkaProperty 추가

---
+++ casper-status/src/main/kotlin/hs/kr/entrydsm/status/infrastructure/kafka/config/KafkaProperty.kt
@@ -0,0 +1,15 @@
+package hs.kr.entrydsm.status.infrastructure.kafka.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding
+
+/**
+ * 카프카 서버 관련 설정
+ */
+@ConfigurationPropertiesBinding
+@ConfigurationProperties("kafka")
+class KafkaProperty(
+    val serverAddress: String,
+    val confluentApiKey: String,
+    val confluentApiSecret: String
+)
