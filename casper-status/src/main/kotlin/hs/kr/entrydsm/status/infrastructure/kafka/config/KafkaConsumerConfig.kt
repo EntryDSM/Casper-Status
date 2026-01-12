57a1631ac81d4c4db5b2718c76bf1deb3c0e492a
coehgns <modooboiroo@gmail.com>
2026-01-12 19:57:16 +0900

    refactor : KafkaConsumerConfig

--- casper-status/src/main/kotlin/hs/kr/entrydsm/status/infrastructure/kafka/config/KafkaConsumerConfig.kt
+++ casper-status/src/main/kotlin/hs/kr/entrydsm/status/infrastructure/kafka/config/KafkaConsumerConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.JsonDeserializer
 
 /**
@@ -31,12 +32,13 @@ class KafkaConsumerConfig(
         return mapOf(
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaProperty.serverAddress,
             ConsumerConfig.ISOLATION_LEVEL_CONFIG to "read_committed",
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
             ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to "false",
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "latest",
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to JsonDeserializer::class.java,
             ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG to 5000,
-            JsonDeserializer.TRUSTED_PACKAGES to "*",
+            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to StringDeserializer::class.java,
+            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to StringDeserializer::class.java,
         )
     }
 }
