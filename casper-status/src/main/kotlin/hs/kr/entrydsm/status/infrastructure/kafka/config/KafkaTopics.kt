43ccf4f306b4caa1e40e0065562da859e5fe7ec8
coehgns <modooboiroo@gmail.com>
2026-01-18 22:37:14 +0900

    refactor ( #26 ) : KafkaTopics

--- casper-status/src/main/kotlin/hs/kr/entrydsm/status/infrastructure/kafka/config/KafkaTopics.kt
+++ casper-status/src/main/kotlin/hs/kr/entrydsm/status/infrastructure/kafka/config/KafkaTopics.kt
@@ -15,4 +15,10 @@ object KafkaTopics {
     const val DELETE_USER = "delete-user"
 
     const val CANCEL_SUBMITTED_APPLICATION = "cancel-submitted-application"
+
+    const val APPLICATION_STATUS_CREATE_COMPLETED = "application-status-create-completed"
+
+    const val APPLICATION_STATUS_CREATE_FAILED = "application-status-create-failed"
+
+    const val USER_RECEIPT_CODE_UPDATE_FAILED = "user-receipt-code-update-failed"
 }
